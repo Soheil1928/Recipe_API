b9925d21e3a862872751a4fb4dc2045a8830d390
soheil.mahjoob <soheil.mahjoob73@gmail.com>
2025-04-02 00:17:50 +0330

    feat(db_app): Edit UserAdmin for create user and add test_create_user_page for it.

--- app/db_app/admin.py
+++ app/db_app/admin.py
@@ -13,12 +13,25 @@ class UserAdmin(BaseUserAdmin):
     """Define the admin page for users."""
     ordering = ['id']
     list_display = ['email', 'name']
+
+    # fieldsets: Used for show information of user.
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
-        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser')}), # noqa
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser')}),  # noqa
         (_('Important dates'), {'fields': ('last_login',)}),
     )
     readonly_fields = ['last_login']
+
+    # add_fieldsets: Used for create of user in admin panel.
+    add_fieldsets = (
+        (None, {'classes': ('wide',), 'fields': ('email',
+                                                 'password1',
+                                                 'password2',
+                                                 'name',
+                                                 'is_active',
+                                                 'is_staff',
+                                                 'is_superuser')}),
+    )
 
 
 # UserAdmin is optional ---> admin.site.register(models.User)
