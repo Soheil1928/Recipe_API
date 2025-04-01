d452f3ac9d165839eb5a71d0db0b48925ae7101e
soheil.mahjoob <soheil.mahjoob73@gmail.com>
2025-04-02 00:07:48 +0330

    feat(db_app): Add UserAdmin and add test_edit_user_page for it.

--- app/db_app/admin.py
+++ app/db_app/admin.py
@@ -1,3 +1,25 @@
-from django.contrib import admin # noqa
+"""
+Django admin customization.
+"""
 
-# Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
+
+from . import models
+
+
+class UserAdmin(BaseUserAdmin):
+    """Define the admin page for users."""
+    ordering = ['id']
+    list_display = ['email', 'name']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser')}), # noqa
+        (_('Important dates'), {'fields': ('last_login',)}),
+    )
+    readonly_fields = ['last_login']
+
+
+# UserAdmin is optional ---> admin.site.register(models.User)
+admin.site.register(models.User, UserAdmin)
